dc171f651a94894efa6c93edb490a870cf7953c9
elVictor <39276857+Elvictor01@users.noreply.github.com>
2020-11-10 16:12:37 +0100

    Update manage.py

--- marketplace/manage.py
+++ marketplace/manage.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
-#import os
-#import sys
+import os
+import sys
 
 
 
-#def main():
+def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'marketplace.settings')
     try:
